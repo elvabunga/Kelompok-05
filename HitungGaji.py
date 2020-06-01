f8a1484f16bebc8d99dddd5f72b255a4e0e03eb4
I0318080-RizkiAnandaPutraNurRohmat <65972153+I0318080-RizkiAnandaPutraNurRohmat@users.noreply.github.com>
2020-06-01 15:20:43 +0700

    Update HitungGaji.py

--- HitungGaji.py
+++ HitungGaji.py
@@ -1,3 +1,4 @@
 print("=========================================================")
 print("PROGRAM HITUNG GAJI BULANAN KARYAWAN")
 print("=========================================================")
+print("1. Menampilkan list karyawan")
