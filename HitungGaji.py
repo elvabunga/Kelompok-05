3e1bdcbecd816f59ff02673d34aa0dd22ed1afd3
I0318080-RizkiAnandaPutraNurRohmat <65972153+I0318080-RizkiAnandaPutraNurRohmat@users.noreply.github.com>
2020-06-01 21:24:43 +0700

    Update HitungGaji.py

--- HitungGaji.py
+++ HitungGaji.py
@@ -11,131 +11,135 @@ lembur=25000
 lemburtglmrh=100000
 isinyadata=len(Jabatan)
 
-print('No.','\t','Jabatan','\t','Gaji Pokok','\t','Tunjangan')
-for i in range(isinyadata):
-    print(i+1,'\t',Jabatan[i],'\t',GajiPokok[i],'\t',Tunjangan[i])
+def header():
+    print('No.','\t','Jabatan','\t','Gaji Pokok','\t','Tunjangan')
+    for i in range(isinyadata):
+        print(i+1,'\t',Jabatan[i],'\t',GajiPokok[i],'\t',Tunjangan[i])
 
-totalkaryawan= int(input('jumlah total karyawan dalam perusahaan : '))
-                     
-for i in range (totalkaryawan):
-    data = []
-    #datanya = (data) 
-    nama = input('Nama Karyawan : ')
-    NIP = input('Nomor Induk Pegawai : ')
-    jbt = int(input('Nomor Jabatan : '))
-    data.append(NIP)
-    data.append(nama)
-    data.append(Jabatan[jbt-1])
-    if jbt == 1:
-        c = float(input('Banyak cuti : '))
-        l = int(input('Banyaknya jam lembur : '))
-        merah = int(input('Banyaknya masuk di tanggal merah atau hari Minggu : '))
-        t = int(input('Banyaknya jam keterlambatan : '))
-        if c <= 2:
-            gpk = GajiPokok[0]
+def proses():
+    totalkaryawan= int(input('jumlah total karyawan dalam perusahaan : '))
+                         
+    for i in range (totalkaryawan):
+        data = []
+        #datanya = (data) 
+        nama = input('Nama Karyawan : ')
+        NIP = input('Nomor Induk Pegawai : ')
+        jbt = int(input('Nomor Jabatan : '))
+        data.append(NIP)
+        data.append(nama)
+        data.append(Jabatan[jbt-1])
+        if jbt == 1:
+            c = float(input('Banyak cuti : '))
+            l = int(input('Banyaknya jam lembur : '))
+            merah = int(input('Banyaknya masuk di tanggal merah atau hari Minggu : '))
+            t = int(input('Banyaknya jam keterlambatan : '))
+            if c <= 2:
+                gpk = GajiPokok[0]
+            else:
+                gpk = GajiPokok[0] - (c-2)*GajiPokok[0]/25
+            lemburan = l * lembur + merah * lemburtglmrh
+            gajikotor = gpk + Tunjangan[0] + lemburan
+            asuransi = 0.02 * gpk
+            pajak = PajakGajiPokok * gpk
+            telat = t * DendaKeterlambatan
+            potongan = telat + asuransi + pajak
+            gajibersih = gajikotor - potongan
+            data.append(gpk)
+            data.append(Tunjangan[0])
+            data.append(lemburan)
+            data.append(gajikotor)
+            data.append(asuransi)
+            data.append(pajak)
+            data.append(telat)
+            data.append(potongan)
+            data.append(gajibersih)
+            #dtkaryawan[nama] = data
+            listPekerja.append(data)
+        elif jbt == 2:
+            c = float(input('Banyak cuti : '))
+            l = int(input('Banyaknya jam lembur : '))
+            merah = int(input('Banyaknya masuk di tanggal merah atau hari Minggu : '))
+            t = int(input('Banyaknya jam keterlambatan : '))
+            if c <= 2:
+                gpk = GajiPokok[1]
+            else:
+                gpk = GajiPokok[1] - (c-2)*GajiPokok[1]/25
+            lemburan = l * lembur + merah * lemburtglmrh
+            gajikotor = gpk + Tunjangan[1] + lemburan
+            asuransi = 0.02 * gpk
+            pajak = PajakGajiPokok * gpk
+            telat = t * DendaKeterlambatan
+            potongan = telat + asuransi + pajak
+            gajibersih = gajikotor - potongan
+            data.append(gpk)
+            data.append(Tunjangan[1])
+            data.append(lemburan)
+            data.append(gajikotor)
+            data.append(asuransi)
+            data.append(pajak)
+            data.append(telat)
+            data.append(potongan)
+            data.append(gajibersih)
+            #dtkaryawan[nama] = data
+            listPekerja.append(data)
+        elif jbt == 3:
+            c = float(input('Banyak cuti : '))
+            l = int(input('Banyaknya jam lembur : '))
+            merah = int(input('Banyaknya masuk di tanggal merah atau hari Minggu : '))
+            t = int(input('Banyaknya jam keterlambatan : '))
+            if c <= 2:
+                gpk = GajiPokok[2]
+            else:
+                gpk = GajiPokok[2] - (c-2)*GajiPokok[2]/25
+            lemburan = l * lembur + merah * lemburtglmrh
+            gajikotor = gpk + Tunjangan[2] + lemburan
+            asuransi = 0.02 * gpk
+            pajak = PajakGajiPokok * gpk
+            telat = t * DendaKeterlambatan
+            potongan = telat + asuransi + pajak
+            gajibersih = gajikotor - potongan
+            data.append(gpk)
+            data.append(Tunjangan[2])
+            data.append(lemburan)
+            data.append(gajikotor)
+            data.append(asuransi)
+            data.append(pajak)
+            data.append(telat)
+            data.append(potongan)
+            data.append(gajibersih)
+            #dtkaryawan[nama] = data
+            listPekerja.append(data)
+        elif jbt == 4:
+            c = float(input('Banyak cuti : '))
+            l = int(input('Banyaknya jam lembur : '))
+            merah = int(input('Banyaknya masuk di tanggal merah atau hari Minggu : '))
+            t = int(input('Banyaknya jam keterlambatan : '))
+            if c <= 2:
+                gpk = GajiPokok[3]
+            else:
+                gpk = GajiPokok[3] - (c-2)*GajiPokok[3]/25
+            lemburan = l * lembur + merah * lemburtglmrh
+            gajikotor = gpk + Tunjangan[3] + lemburan
+            asuransi = 0.02 * gpk
+            pajak = PajakGajiPokok * gpk
+            telat = t * DendaKeterlambatan
+            potongan = telat + asuransi + pajak
+            gajibersih = gajikotor - potongan
+            data.append(gpk)
+            data.append(Tunjangan[3])
+            data.append(lemburan)
+            data.append(gajikotor)
+            data.append(asuransi)
+            data.append(pajak)
+            data.append(telat)
+            data.append(potongan)
+            data.append(gajibersih)
+            #dtkaryawan[nama] = data
+            listPekerja.append(data)
         else:
-            gpk = GajiPokok[0] - (c-2)*GajiPokok[0]/25
-        lemburan = l * lembur + merah * lemburtglmrh
-        gajikotor = gpk + Tunjangan[0] + lemburan
-        asuransi = 0.02 * gpk
-        pajak = PajakGajiPokok * gpk
-        telat = t * DendaKeterlambatan
-        potongan = telat + asuransi + pajak
-        gajibersih = gajikotor - potongan
-        data.append(gpk)
-        data.append(Tunjangan[0])
-        data.append(lemburan)
-        data.append(gajikotor)
-        data.append(asuransi)
-        data.append(pajak)
-        data.append(telat)
-        data.append(potongan)
-        data.append(gajibersih)
-        #dtkaryawan[nama] = data
-        listPekerja.append(data)
-    elif jbt == 2:
-        c = float(input('Banyak cuti : '))
-        l = int(input('Banyaknya jam lembur : '))
-        merah = int(input('Banyaknya masuk di tanggal merah atau hari Minggu : '))
-        t = int(input('Banyaknya jam keterlambatan : '))
-        if c <= 2:
-            gpk = GajiPokok[1]
-        else:
-            gpk = GajiPokok[1] - (c-2)*GajiPokok[1]/25
-        lemburan = l * lembur + merah * lemburtglmrh
-        gajikotor = gpk + Tunjangan[1] + lemburan
-        asuransi = 0.02 * gpk
-        pajak = PajakGajiPokok * gpk
-        telat = t * DendaKeterlambatan
-        potongan = telat + asuransi + pajak
-        gajibersih = gajikotor - potongan
-        data.append(gpk)
-        data.append(Tunjangan[1])
-        data.append(lemburan)
-        data.append(gajikotor)
-        data.append(asuransi)
-        data.append(pajak)
-        data.append(telat)
-        data.append(potongan)
-        data.append(gajibersih)
-        #dtkaryawan[nama] = data
-        listPekerja.append(data)
-    elif jbt == 3:
-        c = float(input('Banyak cuti : '))
-        l = int(input('Banyaknya jam lembur : '))
-        merah = int(input('Banyaknya masuk di tanggal merah atau hari Minggu : '))
-        t = int(input('Banyaknya jam keterlambatan : '))
-        if c <= 2:
-            gpk = GajiPokok[2]
-        else:
-            gpk = GajiPokok[2] - (c-2)*GajiPokok[2]/25
-        lemburan = l * lembur + merah * lemburtglmrh
-        gajikotor = gpk + Tunjangan[2] + lemburan
-        asuransi = 0.02 * gpk
-        pajak = PajakGajiPokok * gpk
-        telat = t * DendaKeterlambatan
-        potongan = telat + asuransi + pajak
-        gajibersih = gajikotor - potongan
-        data.append(gpk)
-        data.append(Tunjangan[2])
-        data.append(lemburan)
-        data.append(gajikotor)
-        data.append(asuransi)
-        data.append(pajak)
-        data.append(telat)
-        data.append(potongan)
-        data.append(gajibersih)
-        #dtkaryawan[nama] = data
-        listPekerja.append(data)
-    elif jbt == 4:
-        c = float(input('Banyak cuti : '))
-        l = int(input('Banyaknya jam lembur : '))
-        merah = int(input('Banyaknya masuk di tanggal merah atau hari Minggu : '))
-        t = int(input('Banyaknya jam keterlambatan : '))
-        if c <= 2:
-            gpk = GajiPokok[3]
-        else:
-            gpk = GajiPokok[3] - (c-2)*GajiPokok[3]/25
-        lemburan = l * lembur + merah * lemburtglmrh
-        gajikotor = gpk + Tunjangan[3] + lemburan
-        asuransi = 0.02 * gpk
-        pajak = PajakGajiPokok * gpk
-        telat = t * DendaKeterlambatan
-        potongan = telat + asuransi + pajak
-        gajibersih = gajikotor - potongan
-        data.append(gpk)
-        data.append(Tunjangan[3])
-        data.append(lemburan)
-        data.append(gajikotor)
-        data.append(asuransi)
-        data.append(pajak)
-        data.append(telat)
-        data.append(potongan)
-        data.append(gajibersih)
-        #dtkaryawan[nama] = data
-        listPekerja.append(data)
-    else:
-        print('data tidak terdeteksi')
-for gaji in listPekerja:
-        print(gaji[0],'\t',gaji[1],'\t',gaji[2],'\t',gaji[3],'\t',gaji[4],'\t',gaji[5],gaji[6],'\t',gaji[7],'\t',gaji[8],'\t',gaji[9],'\t',gaji[10],'\t',gaji[11])
-  
+            print('data tidak terdeteksi')
+    for gaji in listPekerja:
+            print(gaji[0],'\t',gaji[1],'\t',gaji[2],'\t',gaji[3],'\t',gaji[4],'\t',gaji[5],'\t',gaji[6],'\t',gaji[7],'\t',gaji[8],'\t',gaji[9],'\t',gaji[10],'\t',gaji[11])
+
+header()
+proses()
