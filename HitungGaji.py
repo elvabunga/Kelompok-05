cfd249c420354449a5773cab18433a4fc5e375a6
desikaanggii <66167170+desikaanggii@users.noreply.github.com>
2020-06-01 15:25:30 +0700

    Update HitungGaji.py

--- HitungGaji.py
+++ HitungGaji.py
@@ -1,4 +1,8 @@
 print("=========================================================")
 print("PROGRAM HITUNG GAJI BULANAN KARYAWAN")
 print("=========================================================")
-print("1. Menampilkan list karyawan")
+
+
+Golongan = ['IIA','IIIA','IVA']
+Gaji Pokok = [3000000,400000,6000000]
+Tunjangan = [2000000,3000000,5000000]
