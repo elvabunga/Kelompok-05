815432ebe4e3f7b02a5d0179e2b8cfcec4f63414
desikaanggii <66167170+desikaanggii@users.noreply.github.com>
2020-06-01 15:55:02 +0700

    Update HitungGaji.py

--- HitungGaji.py
+++ HitungGaji.py
@@ -6,3 +6,6 @@ print("=========================================================")
 Golongan = ['Direktur','Manager','SPV','Staff','OB']
 Gaji Pokok = [7000000,5000000,3000000,20000000,1000000]
 Tunjangan = [3000000,2000000,1500000,1000000,500000]
+Upah Lembur =  
+Pajak Gaji Pokok =
+Denda Keterlambatan = 
